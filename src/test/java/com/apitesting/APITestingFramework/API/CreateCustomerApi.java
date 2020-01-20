0589f6f5cfe1c2abdf91362bee026e5fd6abad27
myqaprofile17 <myqaprofile17@gmail.com>
2020-01-20 10:55:00 -0500

    second commit

--- src/test/java/com/apitesting/APITestingFramework/API/CreateCustomerApi.java
+++ src/test/java/com/apitesting/APITestingFramework/API/CreateCustomerApi.java
@@ -5,7 +5,7 @@ import static io.restassured.RestAssured.given;
 import java.util.Hashtable;
 
 import io.restassured.response.Response;
-
+/* this is a new commit */
 public class CreateCustomerApi {
 	public static Response sendPostRequestToCreateCustomerAPIwithInvalidAuthKey(Hashtable<String, String> data) {
 		Response response = given().auth().basic("sk_test_lPiNftCegbEnQ76RpMUJcq5p00ByWcJdXt1", "").formParam("name", data.get("name")).formParam("email", data.get("email")).formParam("description", data.get("description")).post("https://api.stripe.com/v1/customers");
